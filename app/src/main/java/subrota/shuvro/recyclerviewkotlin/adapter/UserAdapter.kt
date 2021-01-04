8e5705dfac4e39fed9ef06b2fd33fd0b7fdc2d2c
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-01-05 03:03:29 +0600

    Adapter Created

--- app/src/main/java/subrota/shuvro/recyclerviewkotlin/adapter/UserAdapter.kt
+++ app/src/main/java/subrota/shuvro/recyclerviewkotlin/adapter/UserAdapter.kt
@@ -1,4 +1,33 @@
 package subrota.shuvro.recyclerviewkotlin.adapter
 
-class UserAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import subrota.shuvro.recyclerviewkotlin.R
+import subrota.shuvro.recyclerviewkotlin.adapter.UserAdapter.ViewHolder
+import subrota.shuvro.recyclerviewkotlin.model.UserDataClass
+
+ class UserAdapter(private val users: ArrayList<UserDataClass>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_row_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(users[position])
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bindItem(user: UserDataClass){
+            itemView.findViewById<TextView>(R.id.nameTV).text = user.name
+            itemView.findViewById<TextView>(R.id.occupationTV).text = user.occupation
+        }
+    }
 }
--- app/src/main/java/subrota/shuvro/recyclerviewkotlin/model/UserDataClass.kt
+++ app/src/main/java/subrota/shuvro/recyclerviewkotlin/model/UserDataClass.kt
@@ -1,4 +1,4 @@
 package subrota.shuvro.recyclerviewkotlin.model
 
-class UserDataClass (val name: String, val occupation: String) {
+public class UserDataClass (val name: String, val occupation: String) {
 }
